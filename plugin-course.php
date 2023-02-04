152562f81867cdcb5de59ccb812c973b6d577885
wiktor-bandura <band.wiktor@gmail.com>
2023-02-04 17:49:50 +0100

    Crated class for plugin

--- plugin-course.php
+++ plugin-course.php
@@ -6,8 +6,16 @@
 	Author: Wiktor & Brad
 */
 
-add_filter('the_content', );
-
-function add_to_end_of_post() {
-
+class WordCountAndTimePlugin {
+	function __construct() {
+		add_action('admin_menu', array($this, 'admin_page'));
+	}
+	function admin_page() {
+		add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'settings_page_template'));
+	}
+	function settings_page_template() { ?>
+		Hello world from my plugin
+	<?php }
 }
+
+$word_count_and_time_plugin = new WordCountAndTimePlugin();
