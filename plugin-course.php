fdea1a38bb84bcc9f2c88d8b2ec337e778855d53
wiktor-bandura <band.wiktor@gmail.com>
2023-02-04 17:33:49 +0100

    Set up folder for plugin

---
+++ plugin-course.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Plugin Name: First WordPress Plugin
+	Description: Simple WordPress plugin.
+	Version: 1.0
+	Author: Wiktor & Brad
+*/
+
+add_filter('the_content', );
+
+function add_to_end_of_post() {
+
+}
